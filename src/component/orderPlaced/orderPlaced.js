a7060f4d3d96f9fa92362acb5334fcb121f9de8a
simranjeet1234 <jappandeepsingh04@gmail.com>
2021-05-07 14:35:33 +0530

    10th commit

--- src/component/orderPlaced/orderPlaced.js
+++ src/component/orderPlaced/orderPlaced.js
@@ -4,21 +4,34 @@ import classes from './orderPlaced.module.css';
 import {Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import InfoTable from '../infoTable/infoTable'
+import {ProductConsumer} from '../../context/context';
 
 export default function OrderPlaced() {
     return (
         <React.Fragment>
         <div className={classes.body}>
             <Row>
-                <Col className="col-lg-12 col-md-12 col-12 ">
-                    <div className={classes.placed}>
-                    <h1>Your Order has been placed</h1>
-                    <p>Lorem ipsum dolor amet  sustainable gastropub, echo park actually green juice</p>
-                    <Link to="/">
-                    <Button className={classes.myButton} variant="warning">Continue Shopping</Button>
-                    </Link>
-                    </div>
-                </Col>
+                <ProductConsumer>
+                    {value => {
+                        return(
+                            <Col className="col-lg-12 col-md-12 col-12 ">
+                                <div className={classes.placed}>
+                                <h1>Your Order has been placed</h1>
+                                <p>Lorem ipsum dolor amet  sustainable gastropub, echo park actually green juice</p>
+                                <Link to="/">
+                                <Button 
+                                onClick={() => {value.clearCartHandler()}} 
+                                className={classes.myButton} 
+                                variant="warning"
+                                >Continue Shopping</Button>
+                                </Link>
+                                </div>
+                            </Col>
+
+                        )
+                    }}
+                    
+                </ProductConsumer>  
             </Row>
         </div>
             <InfoTable/>
